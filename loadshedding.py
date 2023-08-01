4d33b76ca6bc78043d4219c3e75cecb32d22f69b
Brendanlearnings <brendanlearnings@gmail.com>
2023-08-01 11:39:26 +0200

    broke

--- loadshedding.py
+++ loadshedding.py
@@ -43,7 +43,7 @@ def main():
     st.title('Loadshedding application to check current schedule for a location')
     # selection = st.text_input('Please enter your area and hit enter:')
     current_schedule = status()
-    st.write(type(current_schedule))
+    # st.write(type(current_schedule))
     st.json(current_schedule)
     
 
